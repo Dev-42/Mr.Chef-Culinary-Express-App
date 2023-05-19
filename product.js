7b3048fa2a1613883f781982a7a72a1385287237
server_42 <devbhattacharya42@gmail.com>
2023-05-20 00:45:19 +0530

    New Commit

--- product.js
+++ product.js
@@ -23,6 +23,8 @@ let cardContainer = document.getElementById('cardContainer');
           let card = document.createElement('div');
           card.style.width = '18rem';
           card.setAttribute('class', 'card h-100');
+          // This will be used for sorting
+          card.setAttribute('data-price',item.price)
 
           let img = document.createElement('img');
           img.setAttribute('class', 'card-img-top');
@@ -40,7 +42,7 @@ let cardContainer = document.getElementById('cardContainer');
           p.innerText = item.description;
 
           let paraPrice = document.createElement('p');
-          paraPrice.innerText = item.price;
+          paraPrice.innerText = "₹" + item.price;
 
           cardBody.append(h5, p, paraPrice);
 
@@ -65,5 +67,59 @@ let arr = JSON.parse(localStorage.getItem('food-items')) || [];
 function storeData(item){
   arr.push(item)
   localStorage.setItem('food-items',JSON.stringify(arr))
+}
+
+// Sorting part
+let sortLowHigh = document.getElementById('sortFrist1221')
+sortLowHigh.addEventListener('click',()=>{
+  sortLow()
+})
+async function sortLow(){
+  let data  = await fetch('http://localhost:3000/foods?&_sort=price&_order=asc')
+  let res = await data.json()
+  displayData(res)
+  console.log(res)
+}
+// Sorting high to low
+let sortHighLow = document.getElementById('sortFrist12212')
+sortHighLow.addEventListener('click',()=>{
+  sortHigh()
+})
+async function sortHigh(){
+  let data = await fetch('http://localhost:3000/foods?&_sort=price&_order=desc')
+  let res = await data.json()
+  displayData(res)
+  console.log(res)
+}
+
+// Filter part
+
+let filterPart = document.getElementById('filterbtn1221')
+filterPart.addEventListener('change',filterFun)
+
+async function filterFun(){
+  let data = await fetch('http://localhost:3000/foods')
+  let res = await data.json()
+  console.log(res)
+
+  let fillArr = res.filter((elm)=>{
+    if(filterPart.value == elm.key){
+      return elm
+    }
+    else if(filterPart.value == ""){
+      return res
+    }
+  })
+  displayData(fillArr)
+}
+
+// Search bar implementation
+let searchBar = document.getElementById('inputSearch1221')
+searchBar.addEventListener('input',searchImp)
+
+async function searchImp(){
+  let data = await fetch(`http://localhost:3000/foods?&q=${searchBar.value}`)
+  let res = await data.json()
+  displayData(res)
 }
 
