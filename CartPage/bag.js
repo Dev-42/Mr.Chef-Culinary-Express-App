f3e34911b1ff2951c0fe037813ed4ef25c37e2fd
server_42 <devbhattacharya42@gmail.com>
2023-06-30 13:24:47 +0530

    Cart Page fully functional

--- CartPage/bag.js
+++ CartPage/bag.js
@@ -1,90 +1,129 @@
-import navbar from './components/navbar.js'
+import navbar from './components/navbar.js';
 
-let nav = document.getElementById('navBar')
-nav.innerHTML = navbar()
+const nav = document.getElementById('navBar');
+const bagContainer = document.getElementById('bagPart1221');
+const itemDetails = document.getElementById('')
+const priceContainer = document.getElementById('price213');
+const items = document.getElementById('item1221');
+const emptyCartURL = '../CartPage/emptyCart.html';
 
-// Cart Page logic
-let bagContainer = document.getElementById('bagPart1221')
-let items = document.getElementById('item1221')
-let clicks = 0;
-let cart = JSON.parse(localStorage.getItem('food-items')) || []
+// Event delegation for remove button clicks
+bagContainer.addEventListener('click', handleRemoveFromBag);
 
-if(cart.length === 0){
-    window.location.href = '../CartPage/emptyCart.html'
+// Initialize the cart and display it
+let cart = getCartFromLocalStorage();
+
+if (cart.length === 0) {
+  window.location.href = emptyCartURL;
+} else {
+  displayCart();
+  calculateTotal();
 }
-else{
-    displayCart()
+
+function getCartFromLocalStorage() {
+  return JSON.parse(localStorage.getItem('food-items')) || [];
 }
-function displayCart(){
-    bagContainer.innerHTML = "";
-    cart.map(function(item){
-        let bagDiv = document.createElement('div')
-        bagDiv.setAttribute('id','bagItem1221')
 
-
-        // Image portion
-        let productImageDiv = document.createElement('div')
-        productImageDiv.setAttribute('id','bagImage1221')
-        let proimg = document.createElement('img')
-        proimg.setAttribute('src',item.img)
-        proimg.setAttribute('id','bagImg1221')
-        productImageDiv.append(proimg)
-
-        // Bag Contents
-
-        let bagContents = document.createElement('div')
-        bagContents.setAttribute('id','bagFlex1221')
-
-        let h3 = document.createElement('h3')
-        h3.setAttribute('id','bagHead1221')
-        h3.textContent = item.title;
-
-        let mainFlex = document.createElement('div')
-        mainFlex.setAttribute('id','quanflex1221')
-
-        let span = document.createElement('span')
-        span.setAttribute('id','span1221')
-        span.textContent = "Quantity";
-
-        let quanBar = document.createElement('div')
-        quanBar.setAttribute('id','quandevBar1221')
-        let imgIconminus = document.createElement('img')
-        imgIconminus.setAttribute('src','https://www.svgrepo.com/show/146105/minus.svg') 
-        imgIconminus.setAttribute('alt','minusButton')
-        imgIconminus.setAttribute('id','minus-btn1221')
-        imgIconminus.setAttribute('class','img-fluid')
-        let spanNumBox = document.createElement('span')
-        spanNumBox.setAttribute('id','num1221')
-        spanNumBox.textContent = "1"
-        let imgIconplus = document.createElement('img')
-        imgIconplus.setAttribute('src','https://img.freepik.com/free-icon/add-button-with-plus-symbol-black-circle_318-48599.jpg')
-        imgIconplus.setAttribute('id','plus-btn1221')
-        let paraPrice = document.createElement('p')
-        paraPrice.setAttribute('id','price1221')
-        paraPrice.textContent = '₹' + item.price;
-
-        mainFlex.append(span,quanBar,imgIconminus,spanNumBox,imgIconplus,paraPrice)
-        let removeCartBtn = document.createElement('button')
-        removeCartBtn.setAttribute('id','delete1221')
-        removeCartBtn.textContent = "Remove from Bag"
-
-        bagContents.append(h3,mainFlex,removeCartBtn)
-        bagDiv.append(productImageDiv,bagContents);
-        
-        removeCartBtn.addEventListener('click',function(){
-            // console.log("Hello")
-            cart.pop()
-            bagDiv.style.display = 'none'
-            // console.log(cart.length)
-            items.textContent = '(' + cart.length + ')'
-        })
-        items.textContent = '(' + cart.length + ')'
-        if(cart.length == 0){
-            window.location.reload = '../CartPage/emptyCart.html'
-            console.log("Hello")
-        }
-        bagContainer.append(bagDiv)
-        localStorage.removeItem('food-items')
-    })
+function updateCartInLocalStorage(cart) {
+  localStorage.setItem('food-items', JSON.stringify(cart));
 }
-// console.log(cart.length)
+
+function displayCart() {
+  bagContainer.innerHTML = '';
+
+  cart.forEach((item) => {
+    const bagDiv = createBagItemElement(item);
+    bagContainer.append(bagDiv);
+  });
+
+  items.textContent = `(${cart.length})`;
+}
+
+function createBagItemElement(item) {
+  const bagDiv = document.createElement('div');
+  bagDiv.setAttribute('id', 'bagItem1221');
+
+  const productImageDiv = document.createElement('div');
+  productImageDiv.setAttribute('id', 'bagImage1221');
+  const proimg = document.createElement('img');
+  proimg.setAttribute('src', item.img);
+  proimg.setAttribute('id', 'bagImg1221');
+  productImageDiv.append(proimg);
+
+  const bagContents = document.createElement('div');
+  bagContents.setAttribute('id', 'bagFlex1221');
+
+  const h3 = document.createElement('h3');
+  h3.setAttribute('id', 'bagHead1221');
+  h3.textContent = item.title;
+
+  const mainFlex = document.createElement('div');
+  mainFlex.setAttribute('id', 'quanflex1221');
+
+  const span = document.createElement('span');
+  span.setAttribute('id', 'span1221');
+  span.textContent = 'Quantity';
+
+  const quanBar = document.createElement('div');
+  quanBar.setAttribute('id', 'quandevBar1221');
+
+  const imgIconminus = document.createElement('img');
+  imgIconminus.setAttribute('src', 'https://www.svgrepo.com/show/146105/minus.svg');
+  imgIconminus.setAttribute('alt', 'minusButton');
+  imgIconminus.setAttribute('id', 'minus-btn1221');
+  imgIconminus.setAttribute('class', 'img-fluid');
+
+  const spanNumBox = document.createElement('span');
+  spanNumBox.setAttribute('id', 'num1221');
+  spanNumBox.textContent = '1';
+
+  const imgIconplus = document.createElement('img');
+  imgIconplus.setAttribute('src', 'https://img.freepik.com/free-icon/add-button-with-plus-symbol-black-circle_318-48599.jpg');
+  imgIconplus.setAttribute('id', 'plus-btn1221');
+
+  const paraPrice = document.createElement('p');
+  paraPrice.setAttribute('id', 'price1221');
+  paraPrice.textContent = `₹${item.price}`;
+
+  mainFlex.append(span, quanBar, imgIconminus, spanNumBox, imgIconplus, paraPrice);
+
+  const removeCartBtn = document.createElement('button');
+  removeCartBtn.setAttribute('id', 'delete1221');
+  removeCartBtn.textContent = 'Remove from Bag';
+
+  bagContents.append(h3, mainFlex, removeCartBtn);
+  bagDiv.append(productImageDiv, bagContents);
+
+  return bagDiv;
+}
+
+function handleRemoveFromBag(event) {
+  if (event.target.id === 'delete1221') {
+    const bagItem = event.target.closest('#bagItem1221');
+    const index = Array.from(bagContainer.children).indexOf(bagItem);
+
+    if (index !== -1) {
+      cart.splice(index, 1);
+      bagItem.style.display = 'none';
+      updateCartInLocalStorage(cart);
+      items.textContent = `(${cart.length})`;
+      calculateTotal();
+
+      if (cart.length === 0) {
+        window.location.href = emptyCartURL;
+      }
+    }
+  }
+}
+
+function calculateTotal() {
+  const totalPrice = cart.reduce((total, item) => total + item.price, 0);
+  priceContainer.textContent = `₹ ${totalPrice}`;
+
+  const priceDet121 = document.getElementById('priceDet121');
+  if (priceDet121) {
+    priceDet121.textContent = `Total (${cart.length} item${cart.length > 1 ? 's' : ''})`;
+  }
+}
+
+nav.innerHTML = navbar();
