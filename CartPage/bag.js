f3580adc0ee10351d8a6fff7e579d1b023e6e2ef
server_42 <devbhattacharya42@gmail.com>
2023-07-03 23:45:34 +0530

    Cart Page price details per item updated

--- CartPage/bag.js
+++ CartPage/bag.js
@@ -122,36 +122,9 @@ function calculateTotal() {
   const minusBtns = document.querySelectorAll('.minus-btn1221');
   const quantityInputs = document.querySelectorAll('.num1221');
   const totalPriceElements = document.querySelectorAll('.price1221');
-  
-  // Iterate over each bag item
-  cart.forEach((item, index) => {
-    let quantity = parseInt(quantityInputs[index].value) || 0;
+  let outputTotal
 
-    plusBtns[index].addEventListener('click', function() {
-      quantity += 1;
-      quantityInputs[index].value = quantity;
-      updateTotalPrice(index, quantity);
-    });
-
-    minusBtns[index].addEventListener('click', function() {
-      quantity = quantity > 1 ? quantity - 1 : 1;
-      quantityInputs[index].value = quantity;
-      updateTotalPrice(index, quantity);
-    });
-
-    updateTotalPrice(index, quantity);
-  });
-
-  function updateTotalPrice(index, quantity) {
-    const pricePerItem = cart[index].price;
-    const totalPrice = pricePerItem * quantity;
-    totalPriceElements[index].textContent = `₹ ${totalPrice}`;
-  }
-
-  // Updating the total price of the cart based upon the quantity
-  const totalPrice = cart.reduce((total, item) => total + item.price, 0);
   itemContainer.innerHTML = '';
-
   let priceDetDiv = document.createElement('div');
   priceDetDiv.setAttribute('id', 'priceDet1221');
   let h4 = document.createElement('h4');
@@ -170,6 +143,9 @@ function calculateTotal() {
   pricePara.setAttribute('id', 'priceDet121');
   pricePara.textContent = `Total (${cart.length} item${cart.length > 1 ? 's' : ''})`;
 
+
+  let totalPrice = cart.reduce((total, item) => total + item.price, 0);
+
   let paraPrice = document.createElement('p');
   paraPrice.setAttribute('id', 'price213');
   paraPrice.textContent = `₹ ${totalPrice}`;
@@ -180,20 +156,58 @@ function calculateTotal() {
 
   totalPriceDiv.append(pricePara, paraPrice);
 
-  cart.forEach((item) => {
+  // Iterate over each bag item
+  cart.forEach((item, index) => {
+    let quantity = parseInt(quantityInputs[index].value) || 0;
+
     let divDetails = document.createElement('div');
     divDetails.setAttribute('id', 'priceDev121');
     let pDet = document.createElement('p');
     pDet.setAttribute('id', 'paraDet121');
     pDet.textContent = item.title;
 
+    
     let pPrice = document.createElement('p');
     pPrice.setAttribute('id', 'pricedet121');
-    pPrice.textContent = `₹ ${item.price}`;
-
+    pPrice.textContent = `₹ ${item.price}`
     divDetails.append(pDet, pPrice);
     itemContainer.append(divDetails, hr, totalPriceDiv, btn);
+
+    plusBtns[index].addEventListener('click', function() {
+      quantity += 1;
+      quantityInputs[index].value = quantity;
+      outputTotal = quantityInputs[index].value
+      console.log(outputTotal)
+      pPrice.textContent = `₹ ${item.price * outputTotal}`;
+
+      updateTotalPrice(index, quantity);
+    });
+
+    minusBtns[index].addEventListener('click', function() {
+      quantity = quantity > 1 ? quantity - 1 : 1;
+      quantityInputs[index].value = quantity;
+      outputTotal = quantityInputs[index].value
+      console.log(outputTotal)
+      pPrice.textContent = `₹ ${item.price * outputTotal}`;
+      updateTotalPrice(index, quantity);
+    });
+    console.log(outputTotal)
+    updateTotalPrice(index, quantity);
   });
+
+  function updateTotalPrice(index, quantity) {
+    const pricePerItem = cart[index].price;
+    const totalPrice = pricePerItem * quantity;
+    totalPriceElements[index].textContent = `₹ ${totalPrice}`;
+  }
+
+  // Updating the total price of the cart based upon the quantity
+ 
+  
+
+  // cart.forEach((item) => {
+    
+  // });
 }
 
 
