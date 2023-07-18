48b8baa44a9c9a7e612940e0d9a015ff09b48780
server_42 <devbhattacharya42@gmail.com>
2023-07-18 23:26:54 +0530

    Signout button functionality added

--- Anas/Components/importheader.js
+++ Anas/Components/importheader.js
@@ -46,7 +46,7 @@ function ans_navbar(){
             <!-- login and cart -->
             <div style="display: flex;" id="ans-login-cart-container">
                 <div id="ans-login-btn-container" class="ans-golden-btn">
-                <a href=""><button id="ans-login-btn">SIGNOUT</button></a>
+                <a href="../Cover Page/cover.html"><button id="ans-login-btn">SIGNOUT</button></a>
                     
                 </div>
                 <div id="ans-login-cart-container">
